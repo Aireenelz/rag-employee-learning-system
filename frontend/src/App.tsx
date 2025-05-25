6f119ca9cf006001612a997186eedf10473d6582
Aireenelz <u2100667@siswa.um.edu.my>
2025-05-25 20:38:14 +0800

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to the Employee Learning System</h1>
+    </div>
+  );
+}
+
+export default App;
