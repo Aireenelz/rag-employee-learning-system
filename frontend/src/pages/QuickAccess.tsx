d03d6ce20ba8e43e8ab70b45e1c48c0da58e22cd
Aireenelz <u2100667@siswa.um.edu.my>
2025-06-11 11:33:32 +0800

    quick access initial

--- frontend/src/pages/QuickAccess.tsx
+++ frontend/src/pages/QuickAccess.tsx
@@ -1,7 +1,16 @@
-const QuickAccess = () => {
+const QuickAccess: React.FC = () => {
     return (
         <div>
-            test QuickAccess
+            {/* Header */}
+            <h1 className="text-2xl font-bold mb-3">
+                Quick Access
+            </h1>
+
+            <div className="flex flex-col h-full w-full border rounded-lg bg-els-mainpanelbackground p-3">
+                test<br/>
+                test<br/>
+                test<br/>
+            </div>
         </div>
     );
 };
